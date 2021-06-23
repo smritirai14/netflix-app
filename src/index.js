d3a24377b2d02470b0eec6e398c91dbc9f134998
smritirai14 <smritirai145@gmail.com>
2021-06-23 10:38:55 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Card  from "./Cards";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <> 
+      <Card
+        imgsrc="https://in.images.search.yahoo.com/search/images;_ylt=AwrPg3a7t9JgcTIAoRC7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3BpdnM-?p=DARK+netflix&fr2=piv-web&fr=mcafee#id=22&iurl=http%3A%2F%2Fwww.brooklynvegan.com%2Ffiles%2F2018%2F01%2Fdark.jpg&action=click"
+        title="A Netflix original series"
+        sname="Dark" 
+        link="https://www.netflix.com/in/title/80100172"
+    />
+      <Card
+        imgsrc="https://www.google.com/search?q=lucifer&rlz=1C1CHBF_enIN945IN945&sxsrf=ALeKk02slwOTu2Glgr7abwfHd4aue5O3gQ:1624418940926&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjSgsKP6KzxAhWO4jgGHXMKD2kQ_AUoAnoECAEQBA&biw=649&bih=646#imgrc=6eKs3VMKZ8hw_M" 
+        title="A Netflix original series"
+        sname="Lucifier" 
+        link="https://www.netflix.com/in/title/80057918"
+    />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+     <Card
+        imgsrc="https://www.google.com/search?q=nevertheless+netflix+series&rlz=1C1CHBF_enIN945IN945&sxsrf=ALeKk001hHIJGa9Z4HdWnpDpY73MNvHdNg:1624418219790&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj62NO35azxAhWP4jgGHSrwDggQ_AUoAnoECAEQBA&biw=649&bih=646#imgrc=5BhtdLPdhdy-bM"
+        title="A Netflix original series"
+        sname="Nevertheless" 
+        link="https://www.netflix.com/in/title/81435649"
+    />
+    </>,
+    document.getElementById("root")
+);
